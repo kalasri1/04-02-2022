6a1526b232e62233d0c7eed2e8b3af13fd93dcd9
Hi <Hi@DESKTOP-CR7BQJC.mshome.net>
2022-02-04 10:40:46 +0530

    First Commit

---
+++ javaDataTypes/src/javaDataTypes/Example1.java
@@ -0,0 +1,19 @@
+package javaDataTypes;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			/**
+			 * write a java program to print customer information(id,name,address)
+			 */
+		int customerId=1225056;
+		String customerName="Srikala";
+		String customerAddress="Kakinada";
+		
+		System.out.println(customerId);
+		System.out.println(customerName);
+		System.out.println(customerAddress);
+	}
+
+}
